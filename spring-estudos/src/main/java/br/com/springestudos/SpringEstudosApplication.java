b1f15742b3f6a882601b1978043359022c7d2c25
GiovanniAg <giovanniaguiar321@gmail.com>
2023-01-17 16:16:38 -0300

    primeiro commit

---
+++ spring-estudos/src/main/java/br/com/springestudos/SpringEstudosApplication.java
@@ -0,0 +1,13 @@
+package br.com.springestudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEstudosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEstudosApplication.class, args);
+    }
+
+}
---
+++ spring-estudos/src/test/java/br/com/springestudos/SpringEstudosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springestudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEstudosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
